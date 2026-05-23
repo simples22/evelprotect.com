4bac466c7761ebd4c5cb054e406759780202feb7
Clervens Pierre <clervens202@gmail.com>
2026-05-23 05:23:35 -0400

    Fix our products page import

--- src/app/(public)/our-products/page.jsx
+++ src/app/(public)/our-products/page.jsx
@@ -1,10 +1,9 @@
 import ProductsHero from "@/components/publics/company/ProductsHero";
-import OurProductsHero from "@/components/publics/products/OurProductsHero";
 
 export default function OurProductsPage() {
   return (
     <main>
-     <ProductsHero />
+      <ProductsHero />
     </main>
   );
 }
